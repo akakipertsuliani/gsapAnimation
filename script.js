fd2b667b9aa53e79e4b829c3258b102f9e03dcc9
akakipertsuliani <akaki@pertsuliani>
2024-03-28 22:53:23 +0400

    NewUpload

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-// Control menu bar
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".menu").addEventListener("click", () => {
-        document.querySelector(".nav-bar").style.right = 0;
-    });
+gsap.registerPlugin(ScrollTrigger);
 
-    document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".nav-bar").style.right = "-50%";
-    });
+let sections = gsap.utils.toArray(".wrapper section");
+let timeLine = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".wrapper",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+    }
 });
 
-// Gsap animation
-gsap.from(".logo", 1, {
-    x: -50,
-    opacity: 0
-});
-
-if (window.innerWidth > 768) {
-    gsap.from(".nav-bar", 1, {
-        x: 50,
-        opacity: 0
-    });
-} else if (window.innerWidth < 768) {
-    gsap.from(".menu", 1, {
-        x: 50,
-        opacity: 0
-    });
-}
+timeLine.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+}).to(".x2", {
+    clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
+    ease: "none",
+})
